7206140727b4935eefb0dff687a88a4edf540011
Midiy <midiy009@mail.ru>
2023-11-30 00:32:51 +0300

    Add database models.

--- web_analizer/main/models.py
+++ web_analizer/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class DemographyEntry(models.Model):
+    region = models.CharField("Region code (usually consisting of two letters).", max_length=8)
+    source = models.CharField("Source from which this data was obtained.", max_length=32)
+    year = models.DateField("The year this record represents the data for.")
+    total_population = models.PositiveBigIntegerField("The total number of people lived in this region during this period.", blank=True, null=True)
+    births = models.PositiveBigIntegerField("The number of people born in this region during this period.", blank=True, null=True)
+    deaths = models.PositiveBigIntegerField("The number of people died in this region during this period.", blank=True, null=True)
+    migration = models.BigIntegerField("The number of people left this region during this period minus the number of people entered this region.", blank=True, null=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["region", "source"])
+        ]
+        unique_together = (("region", "source", "year"),)
